eaf4eb0682a3153524134123a2b952a4a6fba70c
rodrigo.resque <rodrigo.resque@mkdata.com.br>
2023-01-31 23:40:55 -0300

    updt

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
 
-import { MatTableModule } from '@angular/material/table';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
 
@@ -12,7 +12,7 @@ import { CoursesComponent } from './courses/courses.component';
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule
+    AppMaterialModule
   ]
 })
 export class CoursesModule { }
