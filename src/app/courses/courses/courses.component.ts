6abed4c1348a3d0f7b139140e535a4f21c12facd
RodResq <rodrigoresque@yahoo.com.br>
2023-02-08 20:29:20 -0300

    updt

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
 import { catchError, Observable, of } from 'rxjs';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 import { Course } from './../model/course';
@@ -13,11 +14,14 @@ import { CoursesService } from './../services/courses.service';
 export class CoursesComponent implements OnInit {
 
   courses$: Observable<Course[]>;
-  displayedColumns= ['_id', 'name', 'category'];
+  displayedColumns= ['_id', 'name', 'category', 'actions'];
 
   constructor(
     public dialog: MatDialog,
-    private coursesService: CoursesService) {
+    private coursesService: CoursesService,
+    private router: Router,
+    private rooute: ActivatedRoute
+    ) {
     this.courses$ = this.coursesService.list()
       .pipe(catchError(error => {
         this.onError('Error ao carregar cursos')
@@ -26,6 +30,13 @@ export class CoursesComponent implements OnInit {
    }
 
   ngOnInit(): void {
+    // TODO document why this method 'ngOnInit' is empty
+
+  }
+
+  onAdd() {
+    this.router.navigate(['new'], {relativeTo: this.rooute});
+
   }
 
   onError(errorMsg: string) {
