08c086dc3acc4d5ecf785ea5202595b813d7cad6
RodResq <rodrigoresque@yahoo.com.br>
2023-02-08 22:24:02 -0300

    updt

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,8 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
@@ -10,7 +13,10 @@ export class CourseFormComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private snackBar: MatSnackBar,
+    private location: Location) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null]
@@ -23,11 +29,22 @@ export class CourseFormComponent implements OnInit {
   }
 
   onSubmit() {
+    this.service.save(this.form.value)
+      .subscribe(resp => this.onSucces('Curso salvo com sucesso.'), error => this.onError('Error ao salvar.'))
+  }
+
+  private onSucces(message: string) {
+    this.snackBar.open(message, '', {duration: 3000});
+    this.onCancel();
 
   }
 
-  onCancel() {
+  private onError(message: string) {
+    this.snackBar.open(message, '', {duration: 3000});
+  }
 
+  onCancel() {
+    this.location.back();
   }
 
 }
